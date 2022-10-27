dc5921253af7421e1ff6cc9f58923a24de1c7974
Viral Mistry <viralhmistry@gmail.com>
2022-10-27 17:38:52 -0500

    Display image and video

--- app/javascript/react/src/components/EventDetail.jsx
+++ app/javascript/react/src/components/EventDetail.jsx
@@ -12,7 +12,7 @@ const EventDetail = () => {
 
   const fetchItem = () => {
     fetch(`https://fortnite-api.theapinetwork.com/item/get?id=${id}`).then((response) => response.json()).then((data) => {
-      console.log(data.data.item)
+      console.log(data.data.item.media[0])
       setItem(data.data.item)
     })
   }
@@ -26,8 +26,48 @@ const EventDetail = () => {
     <div className="row">
       <div className="col-lg-10 mx-auto">
         <h1> Event Detail </h1>
-          <p> <strong> Name </strong>: {item.name}</p>
-          <p> <strong> Description </strong>: {item.description}</p>
+        <div className="row">
+          <div className="col">
+            <p> <strong> Name </strong>: {item.name} </p>
+            <p> <strong> Type </strong>: {item.type} </p>
+            <p> <strong> Cost </strong>: {item.cost} </p>
+            <p> <strong> Description </strong>: {item.description} </p>
+            {
+              item.ratings &&
+                <div>
+                  <p> <strong> Avg Stars </strong>: { item.ratings['avgStars'] } </p>
+                  <p> <strong> Number Votes </strong>: { item.ratings['numberVotes'] } </p>
+                  <p> <strong> Total Points </strong>: { item.ratings['totalPoints'] } </p>
+                </div>
+            }
+          </div>
+          <div className="col">
+            {
+              item.images &&
+                <div>
+                  <p>
+                    <strong> Image </strong>:
+                    <img src={ item.images['background'] } alt="background image" />
+                  </p>
+                </div>
+            }
+          </div>
+          <div className="row">
+            <div className="col-3">
+              {
+                item.media &&
+                  <div>
+                    <p>
+                      <strong> Video </strong>:
+                        <video width="750" height="500" controls >
+                          <source src={item.media[0]['src']} type={item.media[0]['type']} />
+                        </video>
+                    </p>
+                  </div>
+              }
+            </div>
+          </div>
+        </div>
       </div>
     </div>
   )
